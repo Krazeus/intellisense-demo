7e115621dbc7ef5831e4075cc7af89dd7ab41af1
Miguel <mig_dj@hotmail.com>
2017-01-30 17:27:17 -0600

    remote change

--- app.js
+++ app.js
@@ -8,37 +8,46 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-var data = JSON.parse(fs.readFileSync('data/jsonFile100.json', 'utf8'));
+// var data = JSON.parse(fs.readFileSync('data/jsonFile100.json', 'utf8'));
+var data = JSON.parse(fs.readFileSync('data/demoData1302017.json', 'utf8'));
 
 var Bloodhound = require('bloodhound-js');
 
 server.listen(6742);
 
 var engineList = new Bloodhound({
-	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('v'),
+	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('t'),
+	// datumTokenizer: Bloodhound.tokenizers.whitespace,
+	// datumTokenizer: Bloodhound.tokenizers.obj.whitespace('v'),
 	queryTokenizer: Bloodhound.tokenizers.whitespace,
 	identify: function (obj) { return obj.i; },
 	local: data
 });
 
+//<debug>
+console.log('loadData', data.length);
+//</debug>
+app.all('/', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-	socket.emit('hints', [data[0]]);
-	socket.on('other', function (data) {
-		console.log(data);
-	});
-	socket.on('search', function (data) {
 
+	socket.emit('hints', [data[0]]);
+	socket.on('search', function (data) {
+		//<debug>
+		console.log('debug', data);
+		//</debug>
 		engineList.search(data.value, function (param) {
-			if (param.length > 500) {
-				socket.emit('hints', param.slice(0, 500));
-			} else {
-				socket.emit(param);
-			}
+			var data = (param.length > 500) ? param.slice(0, 500) : param;
+
+			socket.emit('hints', data);
 			console.log('search', data.value, param.length);
 		}, function (param) {
 			console.log('asincrono', arguments);
--- remote.js
+++ remote.js
@@ -10,9 +10,79 @@ var io = require('socket.io')(server);
 var fs = require('fs');
 var sql = require('mssql');
 
+var port = 6742;
+var ip = '192.168.56.99';
+
+
+
+
+var normalize = (function () {
+    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+        to = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+        mapping = {};
+
+    for (var i = 0, j = from.length; i < j; i++) {
+        mapping[from.charAt(i)] = to.charAt(i);
+    }
+
+    return function (str) {
+        var ret = [];
+        for (var i = 0, j = str.length; i < j; i++) {
+            var c = str.charAt(i);
+            if (mapping.hasOwnProperty(str.charAt(i)))
+                ret.push(mapping[c]);
+            else
+                ret.push(c);
+        }
+        return ret.join('');
+    };
+})();
+
+
+
+
+var getIndex = function (text) {
+    // var text = "BAYRÓN-DANIEL&CARRANZA RIVERA"
+    text = text.replace(".", " ").replace("-", " ").replace("_", " ").replace("&", " ").replace("/", " ").replace(",", " ").replace(":", " ").replace(";", " ").replace("%", " ");
+    text = normalize(text);
+    text = text.replace("      ", " ").replace("     ", " ").replace("    ", " ").replace("   ", " ").replace("  ", " ");
+    var matches = text.match(/\b(\w\w\w)/g);
+
+    var array = matches;
+    array = array.sort();
+
+    var letters = array;
+    var combi = [];
+    var temp = "";
+    var letLen = Math.pow(2, letters.length);
+
+    for (var i = 0; i < letLen; i++) {
+        temp = "";
+        for (var j = 0; j < letters.length; j++) {
+            if ((i & Math.pow(2, j))) {
+                temp += letters[j] + "_";
+            }
+        }
+        temp = temp.substring(0, temp.length - 1);
+        if (temp !== "") {
+            combi.push({
+                value: temp,
+                index: letters.length
+            });
+        }
+    }
+    return combi;
+};
+
+
+//word not allowed
+
+
+
+
 var Bloodhound = require('bloodhound-js');
 
-server.listen(6742);
+server.listen(port);
 
 var config = {
     user: 'user',
@@ -20,6 +90,7 @@ var config = {
     server: 'server',
     database: 'database'
 };
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -62,13 +133,24 @@ io.on('connection', function (socket) {
         console.log(data);
     });
     socket.on('search', function (data) {
-        console.log(data);
+
+
         if (data.value) {
-            var validLength = (3 * data.index) + (data.index - 1);
-            if (data.value.length === validLength) {
-                // var query = "SELECT d.UUID_DICTIONARY as i, d.expression as e, d.dictionary_translation as v  FROM MPDV_DICTIONARY_ES d  WHERE d.UUID_DICTIONARY IN (SELECT DISTINCT UUIDDICTIONARY FROM MPD_IDX03ES_DICTIONARY WHERE COMBINED_KEY = 'FIE_LAS_TRX')";
-                var query = "SELECT d.UUID_DICTIONARY as i, d.expression as e, d.dictionary_translation as v  FROM MPDV_DICTIONARY_ES d  WHERE d.UUID_DICTIONARY IN (SELECT DISTINCT UUIDDICTIONARY FROM MPD_IDX0" + data.index + "ES_DICTIONARY WHERE COMBINED_KEY = '" + data.value + "')";
-                sendQuery(config, query);
+            if (data.value.length > 2) {
+                var indexList = getIndex(data.value);
+                // var lastItemIndex = indexList[indexList.length - 1];
+                data = indexList[indexList.length - 1];
+                //<debug>
+                console.log('searching', data);
+                //</debug>
+
+
+                var validLength = (3 * data.index) + (data.index - 1);
+                if (data.value.length === validLength) {
+                    // var query = "SELECT d.UUID_DICTIONARY as i, d.expression as e, d.dictionary_translation as v  FROM MPDV_DICTIONARY_ES d  WHERE d.UUID_DICTIONARY IN (SELECT DISTINCT UUIDDICTIONARY FROM MPD_IDX03ES_DICTIONARY WHERE COMBINED_KEY = 'FIE_LAS_TRX')";
+                    var query = "SELECT d.UUID_DICTIONARY as i, d.expression as e, d.dictionary_translation as v  FROM MPDV_DICTIONARY_ES d  WHERE d.UUID_DICTIONARY IN (SELECT DISTINCT UUIDDICTIONARY FROM MPD_IDX0" + data.index + "ES_DICTIONARY WHERE COMBINED_KEY = '" + data.value + "')";
+                    sendQuery(config, query);
+                }
             }
         }
     });
